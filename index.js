487f3e5b6ca4725ea602e7ef9eb79be8a1138359
ZIPcde <votmoyyashik100@gmail.com>
2024-05-02 14:44:12 +0300

    At end of seminar

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const joi = require('joi');
 
 const filePath = path.join(__dirname, 'users.json');
+let customId = 1;
+
+const scheme = joi.object({
+    firstname: joi.string().min(1).required(),
+    secondName: joi.string().min(1).required(),
+    city: joi.string().min(3),
+    age: joi.number().min(0).max(300).required()
+});
 
 const app = express();
 
@@ -24,13 +33,18 @@ app.get('/users/:id', (req, res) => {
 });
 
 app.put('/users/:id', (req, res) => {
+    const result = scheme.validate(req.body);
+    if (result.error){
+        return res.send({ error: result.error.details[0].message });
+    };
+    
     const users = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-    const user = users.find(user => user.id === parseInt(req.params.id));
+    const user = users.find(user => user.id === Number(req.params.id));
     if (user){
         user.firstname = req.body.firstname;
-        user.secondname = req.body.secondname;
-        user.age = req.body.age;
+        user.secondName = req.body.secondName;
         user.city = req.body.city;
+        user.age = req.body.age;
         fs.writeFileSync(filePath, JSON.stringify(users));
         res.send({ user });
     } else {
@@ -38,5 +52,35 @@ app.put('/users/:id', (req, res) => {
     }
 });
 
+app.post('/users', (req, res) => {
+    const result = scheme.validate(req.body);
+    if (result.error){
+        return res.send({ error: result.error.details[0].message });
+    };
+
+    const users = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    const user = {
+        id: ++customId,
+        firstname: req.body.firstname,
+        secondName: req.body.secondName,
+        city: req.body.city,
+        age: req.body.age
+    };
+        users.push(user);
+        fs.writeFileSync(filePath, JSON.stringify(users));
+        res.send({ user });
+});
+
+app.delete('/users/:id', (req, res) => {
+    const users = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    const userIndex = users.findIndex(user => user.id === Number(req.params.id));
+    if (userIndex >= 0){
+        users.splice(userIndex, 1);
+        fs.writeFileSync(filePath, JSON.stringify(users));
+        res.send({ status: 'ok' });
+    } else {
+        res.send({ error: "User not found!" });
+    }
+});
 
 app.listen(3000, () => console.log("Started on port 3000!"));
